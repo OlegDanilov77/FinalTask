cd5d40e33124f31612a345b1714db7cb1ec5d399
Oleg <nanobyte77@mail.ru>
2022-12-28 13:20:30 +0900

    First relise

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Final work of a GeekBrains student Danilov O.P. for 1 semester
+
+string[] stringarray = { "hello", "2", "world", ":-)" };
+int arrayLenght = stringarray.Length;
+string[] stringarray2 = new string[arrayLenght];
+int index2 = 0;
+
+for (int index = 0; index < arrayLenght; index++)
+{
+    if (stringarray[index].Length < 4)
+    {
+        stringarray2[index2] = stringarray[index];
+        index2++;
+    }
+}
+
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
